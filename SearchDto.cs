a0f524df58f67391b96aeeec8bdb93e1d423b67b
Thumma Jay Outlook <thummaj@outlook.com>
2021-11-21 09:05:39 -0500

    Revert "publish to api git cicd"
    This reverts commit c20f379dac22910b3ada4017efc5f88ca2506f4c.

--- SearchDto.cs
+++ SearchDto.cs
@@ -2,5 +2,6 @@ public class SearchDto
 {
     public string Id { get; set; }
     public string Data { get; set; }
+    //public string SortOrderIndex { get; set; }
     public string DataType { get; set; }
 }
