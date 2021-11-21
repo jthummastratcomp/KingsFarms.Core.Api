c20f379dac22910b3ada4017efc5f88ca2506f4c
Thumma Jay Outlook <thummaj@outlook.com>
2021-11-21 08:01:11 -0500

    publish to api git cicd

--- SearchDto.cs
+++ SearchDto.cs
@@ -2,6 +2,5 @@ public class SearchDto
 {
     public string Id { get; set; }
     public string Data { get; set; }
-    //public string SortOrderIndex { get; set; }
     public string DataType { get; set; }
 }
