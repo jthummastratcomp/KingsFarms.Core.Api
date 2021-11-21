1543fd3d906621dc1c1a4a5bdcfd230fd52d246a
Thumma Jay Outlook <thummaj@outlook.com>
2021-11-21 07:48:53 -0500

    cleanup

--- Program.cs
+++ Program.cs
@@ -29,32 +29,3 @@ app.MapControllers();
 
 app.Run();
 
-
-//public class WebBootstrap : Module
-//{
-//    protected override void Load(ContainerBuilder builder)
-//    {
-//        builder.RegisterType<WeeklyOrdersService>().As<IWeeklyOrdersService>();
-//    }
-//}
-
-//public interface IWeeklyOrdersService
-//{
-//    List<SearchDto> GetInvoiceWeeksListForYear();
-//}
-
-//public class WeeklyOrdersService : IWeeklyOrdersService
-//{
-//    public List<SearchDto> GetInvoiceWeeksListForYear()
-//    {
-//        return new List<SearchDto>(){ new SearchDto(){Data = "hello", DataType = "Hello Dt", Id = "123"}}; //Utils.GetWeeksOfYear(DateTime.Today.Year);
-//    }
-//}
-
-//public class SearchDto
-//{
-//    public string Id { get; set; }
-//    public string Data { get; set; }
-//    //public string SortOrderIndex { get; set; }
-//    public string DataType { get; set; }
-//}
