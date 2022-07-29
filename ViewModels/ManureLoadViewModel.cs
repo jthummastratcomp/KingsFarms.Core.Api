4a1d27da2f6c7be1290d1ef5e5f25c7892441383
Jay Thumma <jthumma@hotmail.com>
2022-07-29 16:24:33 -0400

    manure load display

--- ViewModels/ManureLoadViewModel.cs
+++ ViewModels/ManureLoadViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace KingsFarms.Core.Api.ViewModels;
+﻿using System.Text;
+using KingsFarms.Core.Api.Helpers;
+
+namespace KingsFarms.Core.Api.ViewModels;
 
 public class ManureLoadViewModel
 {
@@ -6,9 +9,24 @@ public class ManureLoadViewModel
     public DateTime ManureLoadDate { get; set; }
     public string ManureLoadDateDisplay => ManureLoadDate.ToString("MM/dd/yyyy");
 
+    public string ManureLoadDisplay
+    {
+        get
+        {
+            if (Loads.Count == 0) return string.Empty;
+
+            if (!Utils.HasRows(Loads)) return string.Empty;
+
+            var builder = new StringBuilder();
+            foreach (var model in Loads) builder.AppendLine($"{model.Key}:{model.Value}");
+
+            return builder.ToString();
+        }
+    }
+
     public void AddFarmLoad(string farmName, int qty)
     {
-        if(string.IsNullOrEmpty(farmName)) return;
+        if (string.IsNullOrEmpty(farmName)) return;
         if (Loads.ContainsKey(farmName))
             Loads[farmName] = qty;
         else Loads.Add(farmName, qty);
